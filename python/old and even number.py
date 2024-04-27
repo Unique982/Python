52f7c803e97a927b42134ea67809c74cf70dbd78
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-04-27 20:15:24 +0545

    Add files via upload
    python start day 1

---
+++ python/old and even number.py
@@ -0,0 +1,6 @@
+# Write a program that reads a number and prints whether it is odd or even.
+a = int(input("Enter a number: "))
+if a%2 ==0:
+    print("The number is even")
+else:
+    print("The number is odd")
